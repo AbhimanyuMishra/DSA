87516818d31cca3692291f4362da9f7ea2294502
Abhimanyu Mishra <99469939+AbhimanyuMishra@users.noreply.github.com>
2026-07-08 23:34:57 +0530

    Time: 2 ms (92.17%) | Memory: 47.9 MB (38.19%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,40 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int size=nums.length;
-        List<Integer> list= new ArrayList<>();
-        for(int i= 0;i<nums.length;i++){
-            if(nums[i]!=0){
-                list.add(nums[i]);
-                size--;
-            }
-
-        }
-        for(int i=0;i<size;i++){
-            list.add(0);
-        }
+        int a=0;
         for(int i=0;i<nums.length;i++){
-            nums[i]=list.get(i);
+            if(nums[i]!=0){
+               
+                nums[a]=nums[i];
+                a++;
+            }
+            
         }
+        for(int i =a; i<nums.length;i++){
+            nums[i]=0;
+        }
+       
         
     }
 }
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int size=nums.length;
+//         List<Integer> list= new ArrayList<>();
+//         for(int i= 0;i<nums.length;i++){
+//             if(nums[i]!=0){
+//                 list.add(nums[i]);
+//                 size--;
+//             }
+
+//         }
+//         for(int i=0;i<size;i++){
+//             list.add(0);
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             nums[i]=list.get(i);
+//         }
+        
+//     }
+// }
+
